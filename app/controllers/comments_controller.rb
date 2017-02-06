5f18c5a1a09a306842a7e01ef956966e0d33a183
Pure99 <pure.denis@gmail.com>
2017-02-06 23:42:48 +0700

    comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    redirect_to edit_post_path(@post)
   end
   
   def destroy
